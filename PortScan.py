d79b1ea7641a3d0d94babb2bde447d5ee0ba4a20
Soku <soku882@gmail.com>
2025-07-26 07:14:42 -0400

    first commit

---
+++ PortScan.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+
+ports = [25,80,53,443,445,8080,8443]
+
+def SynScan(host):
+    ans, unans = sr(IP(dst=host)/TCP(sport=5555,dport=ports,flags="S"),timeout=2,verbose=0)
+    print(f"Open ports at : {host}")
+    for (s,r,) in ans:
+        if s[TCP].dport == r[TCP].sport:
+            print(s[TCP].dport)
+
+
+def DNSScan(host):
+    ans, unans = sr(IP(dst=host)/UDP(sport=5555,dport=53)/DNS(rd=1,qd=DNSQR(qname="google.com")),timeout=2,verbose=0)
+    if ans:
+        print(f"DNS Server at {host}")
+
+host = "192.168.0.1"
+
+SynScan(host)
+DNSScan(host)
